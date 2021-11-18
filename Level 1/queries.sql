f8ec78c86d844188d97e29fc43cff5e049a8fec9
patelveronica <85206817+patelveronica@users.noreply.github.com>
2021-11-18 16:07:02 -0500

    adding final HW

---
+++ Level 1/queries.sql
@@ -0,0 +1,19 @@
+SELECT * FROM review_id_table
+LIMIT 20;
+​
+SELECT COUNT(*) FROM review_id_table;
+​
+SELECT * FROM products
+LIMIT 20;
+​
+SELECT COUNT(*) FROM products;
+​
+SELECT * FROM customers
+LIMIT 20;
+​
+SELECT COUNT(*) FROM customers;
+​
+SELECT * FROM vine_table
+LIMIT 20;
+​
+SELECT COUNT(*) FROM vine_table;
